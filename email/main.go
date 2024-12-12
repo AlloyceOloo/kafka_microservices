959f2f85a26a59ae4ced672ef5e00e638cd1f3a9
AlloyceOloo <alloyceoloo5@gmail.com>
2024-12-12 13:26:24 +0300

    email microservice setup

---
+++ email/main.go
@@ -0,0 +1,20 @@
+package main
+
+//import (
+	//"fmt"
+	//"net/smtp"
+//)
+
+func main() {
+
+
+		//ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
+
+		//smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
+
+		//adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
+
+		//smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+	
+
+}
