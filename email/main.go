2e51357b718d9c983b6bb53ef6a79b157632c358
AlloyceOloo <alloyceoloo5@gmail.com>
2024-12-13 11:09:57 +0300

    Kafka email producer setup

--- email/main.go
+++ email/main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 
 
-		//ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
+		ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
 
-		//smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
 
-		//adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
+		adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
 
-		//smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
 	
 		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 			"bootstrap.servers": "pkc-619z3.us-east1.gcp.confluent.cloud:9092",
