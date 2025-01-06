b336a8ecf88f876709bc90353977d8edbbf2751b
AlloyceOloo <alloyceoloo5@gmail.com>
2025-01-06 16:05:55 +0300

    Email microservice changes

--- email/go.mod
+++ email/go.mod
@@ -2,7 +2,4 @@ module email
 
 go 1.18
 
-require (
-	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
-	github.com/confluentinc/confluent-kafka-go/v2 v2.6.1 // indirect
-)
+require github.com/confluentinc/confluent-kafka-go/v2 v2.6.1
--- email/main.go
+++ email/main.go
@@ -4,26 +4,18 @@ import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"time"
-	//"net/smtp"
+	"encoding/json"
+	"net/smtp"
 )
 
 func main() {
-
-
-		ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
-
-		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
-
-		adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
-
-		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
 	
 		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 			"bootstrap.servers": "pkc-619z3.us-east1.gcp.confluent.cloud:9092",
 			"security.protocol": "SASL_SSL",
 			"sasl.username": "TYNKLLJMPGCT4C3L",
 			"sasl.password": "kUp7KuyBiAH/f2+ltEPaRQs1C93uT5A4HzCejxujZ9FROckxq3HVSI2bmgk8fJxG",
-			"sasl.mechanism": "PLAIN",
+			"sasl.mechanisms": "PLAIN",
 			"group.id":          "myGroup",
 			"auto.offset.reset": "earliest",
 		})
@@ -43,16 +35,31 @@ func main() {
 
 		for run {
 			msg, err := consumer.ReadMessage(time.Second)
-			if err == nil {
-					fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-			} else if !err.(kafka.Error).IsTimeout() {
-				// The client will automatically try to recover from all errors.
-				// Timeout is not considered an error because it is raised by
-				// ReadMessage in absence of messages.
-				fmt.Printf("Consumer error: %v (%v)\n", err, msg)
-			}
+			if err != nil {
+					//fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+					// The client will automatically try to recover from all errors.
+					// Timeout is not considered an error because it is raised by
+					// ReadMessage in absence of messages.
+					fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+					return
+			} 
+
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+
+			var message map[string]interface{}
+
+			json.Unmarshal(msg.Value, &message)
+
+			ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", message["ambassador_revenue"].(float64), message["code"]))
+
+			smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{message["ambassador_email"].(string)}, ambassadorMessage)
+
+			adminMessage := []byte(fmt.Sprintf("Order #%f with a total of $%f has been completed", message["id"].(float64), message["admin_revenue"].(float64)))
+
+			smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+	
 		}
 
 		consumer.Close()
 
-}
+} 
